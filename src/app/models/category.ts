b8de8e5d1ea36ce3d08ac865c6fce653307a101b
yuzukit <yuzuki.t0824@gmail.com>
2022-12-15 14:53:48 +0900

    add first models

---
+++ src/app/models/category.ts
@@ -0,0 +1,12 @@
+export interface ViewValueCategory { // 表示する内容
+    id:    number,
+    name:  string,
+    slug:  string,
+    color: string
+}
+
+export interface ValueCreateCategory { // Todoの登録や更新の内容
+    name:  string,
+    slug:  string,
+    color: string
+}
---
+++ src/app/models/todo.ts
@@ -0,0 +1,15 @@
+export interface ViewValueTodo { // 表示する内容
+    id:             number,
+    title:          string,
+    body:           string,
+    state:          string,
+    category_name?: string,
+    color?:         string
+}
+
+export interface ValueCreateTodo { // Todoの登録や更新の内容
+    category_id: number,
+    title:       string,
+    body:        string,
+    state:       number
+}
